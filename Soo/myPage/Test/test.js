36833fc90470d1dbd34583bee9a902487d9ab226
Victory-soo <victory-soo@naver.com>
2022-08-08 13:20:58 +0900

    Soo

--- Soo/myPage/Test/test.js
+++ Soo/myPage/Test/test.js
@@ -23,44 +23,44 @@ initTexts(pTag4, textArr4);
 initTexts(pTag5, textArr5);
 
 function initTexts(element, textArray) {
-    textArray.push(...textArray);
-    for (let i = 0; i < textArray.length; i++) {
-        element.innerText += `${textArray[i]}\u00A0`;
-    }
+  textArray.push(...textArray);
+  for (let i = 0; i < textArray.length; i++) {
+    element.innerText += `${textArray[i]}\u00A0`;
+  }
 }
 
 function marqueeText(count, element, direction) {
-    if (count > element.scrollWidth / 2) {
-        element.style.transform = `translate3d(0, 0, 0)`;
-        count = 0;
-    }
-    element.style.transform = `translate3d(${direction * count}px, 0, 0)`;
+  if (count > element.scrollWidth / 2) {
+    element.style.transform = `translate3d(0, 0, 0)`;
+    count = 0;
+  }
+  element.style.transform = `translate3d(${direction * count}px, 0, 0)`;
 
-    return count;
+  return count;
 }
 
 function animate() {
-    count1++;
-    count2++;
-    count3++;
-    count4++;
-    count5++;
+  count1++;
+  count2++;
+  count3++;
+  count4++;
+  count5++;
 
-    count1 = marqueeText(count1, pTag1, -1);
-    count2 = marqueeText(count2, pTag2, 1);
-    count3 = marqueeText(count3, pTag3, -1);
-    count4 = marqueeText(count4, pTag4, 1);
-    count5 = marqueeText(count5, pTag5, -1);
+  count1 = marqueeText(count1, pTag1, -1);
+  count2 = marqueeText(count2, pTag2, 1);
+  count3 = marqueeText(count3, pTag3, -1);
+  count4 = marqueeText(count4, pTag4, 1);
+  count5 = marqueeText(count5, pTag5, -1);
 
-    window.requestAnimationFrame(animate);
+  window.requestAnimationFrame(animate);
 }
 
 function scrollHandler() {
-    count1 += 25;
-    count2 += 25;
-    count3 += 25;
-    count4 += 25;
-    count5 += 25;
+  count1 += 25;
+  count2 += 25;
+  count3 += 25;
+  count4 += 25;
+  count5 += 25;
 }
 
 window.addEventListener("scroll", scrollHandler);
@@ -81,22 +81,22 @@ const moveUp = document.querySelector(".moveUp");
 const pageNum = document.getElementsByClassName("pg").length;
 
 moveDown.addEventListener("click", function (e) {
-    if (countBtn < pageNum && countBtn > 1) {
-        countBtn++;
-        moveDown.href = `#p${countBtn}`;
-    } else if (countBtn == 1) {
-        moveDown.href = "#p2";
-        countBtn = 2;
-    }
+  if (countBtn < pageNum && countBtn > 1) {
+    countBtn++;
+    moveDown.href = `#p${countBtn}`;
+  } else if (countBtn == 1) {
+    moveDown.href = "#p2";
+    countBtn = 2;
+  }
 });
 
 moveUp.addEventListener("click", function (e) {
-    if (countBtn >= 2 && countBtn <= pageNum) {
-        countBtn--;
-        moveUp.href = `#p${countBtn}`;
-    } else if (countBtn == 1) {
-        moveUp.href = `#p${countBtn - 1}`;
-    }
+  if (countBtn >= 2 && countBtn <= pageNum) {
+    countBtn--;
+    moveUp.href = `#p${countBtn}`;
+  } else if (countBtn == 1) {
+    moveUp.href = `#p${countBtn - 1}`;
+  }
 });
 
 // BangMangE
@@ -108,46 +108,11 @@ const deg = 360 / len;
 const names = ["red1", "orange1", "yellow1", "green1", "sky1", "navy1", "violet1", "black1"];
 
 for (let i = 0; i < len; i++) {
-    list[i].style.transform = `rotate(${deg * i}deg) translateY(-10vh)`;
+  list[i].style.transform = `rotate(${deg * i}deg) translateY(-10vh)`;
 
-    const pic = list[i].querySelector(".pic");
-    pic.style.backgroundImage = `url("../photo/${names[i]}.png")`;
+  const pic = list[i].querySelector(".pic");
+  pic.style.backgroundImage = `url("../photo/${names[i]}.png")`;
 
-    const title = list[i].querySelector(".text>h2");
-    title.innerHTML = `${names[i]}`;
+  const title = list[i].querySelector(".text>h2");
+  title.innerHTML = `${names[i]}`;
 }
-
-const prev = document.querySelector(".btnPrev");
-const next = document.querySelector(".btnNext");
-let num = 0;
-let active = 0;
-
-prev.addEventListener("click", function (e) {
-    frame.style.transform = `rotate(${deg * ++num}deg)`;
-
-    if (active === 0) {
-        active = len - 1;
-    } else {
-        active--;
-    }
-
-    for (let el of list) {
-        el.classList.remove("on");
-    }
-    list[active].classList.add("on");
-});
-
-next.addEventListener("click", function (e) {
-    frame.style.transform = `rotate(${deg * --num}deg)`;
-
-    if (active === len - 1) {
-        active = 0;
-    } else {
-        active++;
-    }
-
-    for (el of list) {
-        el.classList.remove("on");
-    }
-    list[active].classList.add("on");
-});
