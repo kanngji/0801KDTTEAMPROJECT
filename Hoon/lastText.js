8e0b18f0f6891fd5e07014500dd57542a18dcfe1
kanngji <50470748+kanngji@users.noreply.github.com>
2022-08-10 17:40:50 +0900

    text 날라오기

--- Hoon/lastText.js
+++ Hoon/lastText.js
@@ -8,7 +8,7 @@ tl.from(".part-1", {
 })
   .from(".part-2", {
     x: 1000,
-    duration: 2,
+    duration: 1,
   })
   .fromTo(".commet >img", 0.6, { opacity: 0 }, { opacity: 1, delay: 0.6 })
   .from(".part-3", {
@@ -17,6 +17,6 @@ tl.from(".part-1", {
   })
   .from(".part-4", {
     x: 1000,
-    duration: 2,
+    duration: 1,
   })
   .fromTo(".monster >img", 1.0, { opacity: 0 }, { opacity: 1, delay: 1.0 });
