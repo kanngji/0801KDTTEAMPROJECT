a9cb0e71b6b705dc3102922415107fd3d9bd82f9
Victory-soo <victory-soo@naver.com>
2022-08-12 19:02:00 +0900

    Soo

--- COMPLETE/main.js
+++ COMPLETE/main.js
@@ -11,109 +11,159 @@ const h1 = gsap.timeline();
 
 h1.from(".h1", {
   scale: 3,
-  duration: 2.5,
+  duration: 100,
+  delay: 30,
   opacity: 0,
 })
   .to(
     ".h1",
     {
-      duration: 1,
+      duration: 10,
       x: "45vw",
       y: "-10vw",
+      delay: 30,
     },
     "<"
+  )
+  .from(
+    ".t1",
+    {
+      opacity: 0,
+      duration: 100,
+      delay: 30,
+    },
+    ">"
   )
   .to(".h1", {
     opacity: 1,
     scale: 5,
-    duration: 2.2,
+    duration: 100,
     x: "75vw",
     y: "5vw",
+    delay: 30,
+  })
+  .to(".h1", {
+    opacity: 1,
+    scale: 5,
+    duration: 100,
+    x: "75vw",
+    y: "5vw",
+    delay: 30,
   })
   .to(
     ".t1",
     {
-      duration: 5,
+      duration: 100,
       opacity: 0,
       y: -100,
     },
     "<"
   )
+  .from(
+    ".t2",
+    {
+      opacity: 0,
+      duration: 100,
+      delay: 30,
+    },
+    ">"
+  )
   .to(".h1", {
     scale: 0.8,
-    duration: 2,
+    duration: 100,
+    delay: 30,
     x: "20vw",
     y: "-3vw",
   })
   .to(
     ".t2",
     {
-      duration: 5,
+      duration: 100,
       opacity: 0,
       y: -100,
+      delay: 30,
     },
     "<"
   )
+  .from(".t3", {
+    opacity: 0,
+    duration: 100,
+    delay: 30,
+  })
   .to(".h1", {
     scale: 1.4,
-    duration: 2,
+    duration: 100,
     x: "40vw",
     y: "3vw",
+    delay: 30,
   })
   .to(
     ".t3",
     {
-      duration: 5,
+      duration: 100,
       opacity: 0,
       y: -100,
+      delay: 30,
     },
     "<"
   )
+  .from(".t4", {
+    opacity: 0,
+    duration: 100,
+    delay: 30,
+  })
   .to(".h1", {
-    duration: 1,
+    duration: 100,
     x: "70vw",
     y: "5vw",
+    delay: 30,
   })
+
   .to(
     ".t4",
     {
-      duration: 5,
+      duration: 100,
       opacity: 0,
       y: -100,
+      delay: 30,
     },
     "<"
   )
   .to(".h1", {
-    duration: 1,
+    duration: 100,
     x: "10vw",
     y: "20vw",
   })
-  .to(
-    ".t5",
-    {
-      duration: 5,
-      opacity: 0,
-      y: -100,
-    },
-    "<"
-  )
+
   .to(".h1", {
-    duration: 1,
+    duration: 100,
     rotate: "-110deg",
   })
   .to(".h1", {
-    duration: 2,
+    duration: 100,
     x: "70vw",
     y: "-3vw",
   })
+
   .to(".h1", {
-    duration: 2,
+    duration: 100,
     x: "47.5vw",
     y: "-7vw",
     rotate: "-360deg",
   })
+  .from(".t5", {
+    opacity: 0,
+    duration: 100,
+    delay: 30,
+  })
+  .to(".t5", {
+    duration: 100,
+    opacity: 0,
+    y: -100,
+    delay: 30,
+  })
   .to(".h1", {
-    duration: 2,
+    duration: 100,
     x: "47.5vw",
     y: "37vw",
     opacity: 0,
@@ -124,7 +174,7 @@ ScrollTrigger.create({
   trigger: ".intro",
   start: "3.5% 14%",
   // end: "top 20%",
-  end: "+=5000",
+  end: "+=10000",
   scrub: true,
   markers: true,
   pin: true,
